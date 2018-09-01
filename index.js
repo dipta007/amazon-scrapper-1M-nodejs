5f05f4286d02090522f332d41a2003b9791279e7
Shubhashis Roy Dipta <iamdipta@gmail.com>
2018-09-01 21:59:55 +0600

    debugging purpose

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function getAnother(page) {
         return now.map(nw => nw ? nw.getAttribute('data-asin') : null)
     })
 
+    print(asins)
     asins.forEach(item => {
         if(item)
             allAsins.push(item)
